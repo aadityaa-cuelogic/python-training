abbdc42577ca01deb8c4df55cc1f174aa820ba78
Aaditya Agrawal <aaditya.agrawal@cuelogic.com>
2016-12-27 13:25:19 +0530

    Exercise 2: Comments

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by python
+
+print "I could have code like this" #add the comment after is ignored
+
+# print "jellllsdfdsf dfdsf will not run"
+
+print "This will run"

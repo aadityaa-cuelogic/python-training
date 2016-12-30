6a0564cf0ddf529a21d4aff48093cb24d9d30e1b
Aaditya Agrawal <aaditya.agrawal@cuelogic.com>
2016-12-30 15:51:50 +0530

    If statement

---
+++ ex29.py
@@ -0,0 +1,20 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats :
+    print "Too many cats!"
+if people > cats :
+    print "Not too many cats!"
+if people < dogs :
+    print "Too many dogs"
+if people > dogs :
+    print "Not too many dogs"
+
+dogs += 5
+if people >= dogs :
+    print "People >= dogs"
+if people <= dogs :
+    print "People <= dogs"
+if people == dogs :
+    print "People = dogs"

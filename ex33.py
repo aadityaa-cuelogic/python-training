c390cb6d70cccba09b3eca8da520b4fcce12ad3f
Aaditya Agrawal <aaditya.agrawal@cuelogic.com>
2016-12-30 18:16:34 +0530

    while loop prg

---
+++ ex33.py
@@ -0,0 +1,10 @@
+i = 0
+numbers = []
+
+# while i < 8:
+while i in range(-1,6):
+    print "At tht top i is %d" % i
+    numbers.append(i)
+    i += 1
+    print "Numbers now : ", numbers
+    print "At the bottom i is %d" % i
